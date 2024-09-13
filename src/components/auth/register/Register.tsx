24692059dcf36d338df7eed58a7109bb63c3e87c
David Francisco Blandón Mena <164013609+franccoina@users.noreply.github.com>
2024-09-13 17:05:22 -0500

    Fixing Register.tsx

--- src/components/auth/register/Register.tsx
+++ src/components/auth/register/Register.tsx
@@ -1,47 +1,31 @@
-'use client';
-import { useState } from 'react';
+"use client";
+import { FormEvent, useState } from 'react';
 import { useRouter } from 'next/navigation';
 import { useDispatch, useSelector } from 'react-redux';
-import { registerUser } from '../../../app/redux/slices/authSlice';
+import { loginUser } from '@/src/app/redux/slices/authSlice'; 
 import { toast } from 'react-toastify';
-import { AppDispatch, RootState } from '../../../app/redux/store';
-import InputSignUp from "../../ui/inputs/InputSignUp"
-import { DivUserData, TitleUserData, DivUserInput, DivUserTitle } from "./RegisterStyling"
-import Label from "../../ui/labels/LabelAuth";
-import Select from "../../ui/selects/SelectRegister";
-import TextArea from "../../ui/textAreas/TextAreaRegister";
+import { AppDispatch } from '@/src/app/redux/store';
+import styled from 'styled-components';
+import InputSingUp from '../../ui/inputs/InputSignUp';
 import ButtonSingUp from '../../ui/buttons/ButtonSingUp';
+import Label from '../../ui/labels/LabelAuth';
 
 //Syled
-import { FormWrapper } from '../login/LoginStyling';
-import { Container } from '../login/LoginStyling';
-import { Title } from '../login/LoginStyling';
-import { DivButton } from '../login/LoginStyling';
+import {FormWrapper} from './LoginStyling'
+import {Container} from './LoginStyling'
+import {Title} from './LoginStyling'
+import {DivButton} from './LoginStyling'
 
-export default function RegisterPage() {
-  const dispatch = useDispatch<AppDispatch>();
+export default function LoginPage() {
   const router = useRouter();
-  const { loading, error } = useSelector((state: RootState) => state.auth);
+  const dispatch: AppDispatch = useDispatch(); // Tipado correcto de dispatch
+  const { loading } = useSelector((state: any) => state.auth);
 
+  // State para manejar el formulario
   const [form, setForm] = useState({
-    name: "",
-    lastName: "",
-    age: "",
-    image: "",
-    phoneNumber: "",
-    email: "",
-    password: "",
-    jobTitle: "",
-    description: "",
-    linkedIn: "",
-    behance: "",
-    github: "",
-    createdAt: ""
+    email: '',
+    password: '',
   });
-
-  const [selectedOption, setSelectedOption] = useState<string>("");
-  const [skills, setSkills] = useState<string>("");
-  const [currentStep, setCurrentStep] = useState(0); // Controla la vista actual del carrusel
 
   // Manejar cambios en los inputs
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -51,269 +35,67 @@ export default function RegisterPage() {
     });
   };
 
-  // Manejar el select
-  const handleSelectChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-    setSelectedOption(event.target.value);
-  };
+  // Manejar el submit del formulario de login
+  async function handleSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
 
-  // Manejar el textarea
-  const handleTextAreaChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
-    setSkills(event.target.value);
-  };
+    const { email, password } = form;
 
-  // Manejar el envío del formulario
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-
-    // Validación básica de campos
-    if (!form.email || !form.name || !form.password || !form.lastName || !form.age || !form.description || !form.jobTitle) {
+    if (!email || !password) {
       toast.error('Por favor, completa todos los campos.');
       return;
     }
 
-    try {
-      await dispatch(registerUser(form)).unwrap();
-      toast.success('Registro exitoso!');
-    } catch (err: any) {
-      if (err?.message) {
-        toast.error(`Registro fallido: ${err.message}`);
-      } else {
-        toast.error('Registro fallido. Inténtalo de nuevo.');
+    const resultAction = await dispatch(loginUser({ email, password }));
+
+    if (loginUser.fulfilled.match(resultAction)) {
+      const token = resultAction.payload?.access_token;
+      if (token) {
+        toast.success('Login exitoso!');
+        localStorage.setItem('authToken', token);
+        router.push('/');
       }
+    } else {
+      toast.error('Error al intentar iniciar sesión.');
     }
-  };
-
-  // Renderizar el input actual basado en la vista
-  const renderStep = () => {
-    switch (currentStep) {
-      case 0:
-        return (
-          <>
-            <div>
-              <Title>Registro</Title>
-              <Label
-                text="Email"
-                htmlFor=''
-              />
-              <InputSignUp
-                type="email"
-                name="email"
-                placeholder="Escribe tu nombre..."
-                value={form.email}
-                onChange={handleChange}
-                required
-              />
-              <Label
-                text="Contraseña"
-                htmlFor='password'
-              />
-              <InputSignUp
-                type="password"
-                name="password"
-                placeholder="Escribe tu contraseña..."
-                value={form.password}
-                onChange={handleChange}
-                required
-              />
-            </div>
-          </>
-
-        );
-      case 1:
-        return (
-          <DivUserData>
-            <DivUserTitle>
-              <Title>Tus datos</Title>
-            </DivUserTitle>
-            <DivUserInput>
-              <Label htmlFor="name" text="Nombre*" />
-              <InputSignUp
-                type="text"
-                name="name"
-                placeholder="Escribe tu nombre"
-                value={form.name}
-                onChange={handleChange}
-                required
-              />
-            </DivUserInput>
-            <DivUserInput>
-              <Label htmlFor="lastname" text="Apellidos*" />
-              <InputSignUp
-                type="text"
-                name="lastName"
-                placeholder="Escribe tus apellidos"
-                value={form.lastName}
-                onChange={handleChange}
-                required
-              />
-            </DivUserInput>
-          </DivUserData>
-        );
-      case 2:
-        return (
-          <DivUserData>
-            <DivUserTitle>
-              <Title>Tus datos</Title>
-            </DivUserTitle>
-            <DivUserInput>
-              <Label htmlFor="age" text="Edad" />
-              <InputSignUp
-                type="text"
-                name="age"
-                placeholder="Escribe tu edad"
-                value={form.age}
-                onChange={handleChange}
-                required
-              />
-            </DivUserInput>
-            <DivUserInput>
-              <Label htmlFor="image" text="Imagen" />
-              <InputSignUp
-                type="text"
-                name="image"
-                placeholder="Escribe la url de tu imagen"
-                value={form.image}
-                onChange={handleChange}
-              />
-            </DivUserInput>
-          </DivUserData>
-        );
-      case 3:
-        return (
-          <DivUserData>
-            <DivUserTitle>
-              <TitleUserData>Tus habilidades</TitleUserData>
-            </DivUserTitle>
-            <DivUserInput>
-              <Label htmlFor="area" text="Selecciona una categoría" />
-              <Select
-                value={selectedOption}
-                onChange={handleSelectChange}
-                ariaLabel="Select area"
-                name="area"
-                required
-              />
-            </DivUserInput>
-            <DivUserInput>
-              <Label htmlFor="skills" text="Habilidades" />
-              <TextArea
-                value={skills}
-                onChange={handleTextAreaChange}
-                ariaLabel="Escribe tus habilidades"
-                name="skills"
-                placeholder="Escribe aquí tus habilidades (máx. 200 caracteres)..."
-                required
-                maxLength={200} // Especificamos el límite de caracteres
-              />
-              <p>{skills.length} / 200 caracteres</p> {/* Mostrar contador de caracteres */}
-            </DivUserInput>
-          </DivUserData>
-        );
-      case 4:
-        return (
-          <DivUserData>
-            <DivUserTitle>
-              <TitleUserData>Sobre tu experiencia</TitleUserData>
-            </DivUserTitle>
-            <DivUserInput>
-              <Label htmlFor="jobTitle" text="Trabajo/título" />
-              <InputSignUp
-                type="text"
-                name="jobTitle"
-                placeholder="Nombre de tu trabajo"
-                value={form.jobTitle}
-                onChange={handleChange}
-                required
-              />
-            </DivUserInput>
-            <DivUserInput>
-              <Label htmlFor="description" text="Descripción" />
-              <InputSignUp
-                type="text"
-                name="description"
-                placeholder="Describe tu experiencia "
-                value={form.description}
-                onChange={handleChange}
-                required
-              />
-            </DivUserInput>
-
-          </DivUserData>
-        );
-      case 5:
-        return (
-          <DivUserData>
-            <DivUserTitle>
-              <TitleUserData>Contacto</TitleUserData>
-            </DivUserTitle>
-            <DivUserInput>
-              <Label htmlFor="linkedIn" text="LinkedIn" />
-              <InputSignUp
-                type="text"
-                name="linkedIn"
-                placeholder="Escribe tu linkedIn"
-                value={form.linkedIn}
-                onChange={handleChange}
-
-              />
-            </DivUserInput>
-            <DivUserInput>
-              <Label htmlFor="behance" text="Behance" />
-              <InputSignUp
-                type="text"
-                name="behance"
-                placeholder="Escribe tu behance"
-                value={form.behance}
-                onChange={handleChange}
-              />
-            </DivUserInput>
-            <DivUserInput>
-              <Label htmlFor="github" text="Github" />
-              <InputSignUp
-                type="text"
-                name="github"
-                placeholder="Escribe tu GitHub"
-                value={form.github}
-                onChange={handleChange}
-              />
-            </DivUserInput>
-
-          </DivUserData>
-
-
-        );
-      default:
-        return null;
-    }
-  };
+  }
 
   return (
     <Container>
       <FormWrapper>
-
-        <form onSubmit={handleSubmit}>
-          {renderStep()}
+      <Title>Iniciar Sesion</Title>
+      <form onSubmit={handleSubmit}>
+        <Label
+          text= "Email"
+          htmlFor='email'
+          />
+        <InputSingUp
+          type="email"
+          name="email"
+          placeholder="Email"
+          value={form.email}  
+          required
+          />
+        <Label
+          text= "Contraseña"
+          htmlFor='password'
+          />  
+        <InputSingUp
+          type="password"
+          name="password"
+          placeholder="Contraseña"
+          value={form.password}
+          onChange={handleChange}
+          required
+          />
           <DivButton>
-            {currentStep > 0 && (
-              <ButtonSingUp type="button" onClick={() => setCurrentStep(currentStep - 1)}>
-                ATRÁS
-              </ButtonSingUp>
-            )}
-
-            {currentStep < 5 ? (
-              <ButtonSingUp type="button" onClick={() => setCurrentStep(currentStep + 1)}>
-                SIGUIENTE
-              </ButtonSingUp>
-            ) : (
-              <ButtonSingUp type="submit" disabled={loading}>
-                {loading ? 'Registrando...' : 'ENVIAR'}
-              </ButtonSingUp>
-            )}
+            <ButtonSingUp type="submit" disabled={loading}>
+              {loading ? 'Cargando...' : 'ENTRAR'}
+            </ButtonSingUp>
           </DivButton>
-        </form>
-        {error && <p style={{ color: 'red' }}>{error}</p>}
-      </FormWrapper>
-    </Container>
+      </form>
+          </FormWrapper>
 
+    </Container>
   );
 }
