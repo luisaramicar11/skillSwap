f70d08a78288471860d49ee3e801b831d80c11a1
JoanZapata05 <zapatajoan05@gmail.com>
2024-09-19 12:28:22 -0500

    Creation of the search in discover

--- src/app/user/discover/page.tsx
+++ src/app/user/discover/page.tsx
@@ -1,23 +1,34 @@
 "use client";
+import React, { useState } from "react";
 import Carousel from "../../../components/carousels/CarouseDiscover";
 import AllUsers from "../../../components/AllUsers";
-import Search from "../../../components/searchs/search";
-import { H2, DivContainer, DivRecentUsers } from "./DiscoverStyling";
+import Search from "@/src/components/searchs/search";
+import { H2, DivContainer } from "./DiscoverStyling";
+import { users } from "../../../components/AllUsers";
 
-const handleSearch = () => {
-  console.log("");
-};
 const Discover = () => {
+  
+
+  const [filteredUsers, setFilteredUsers] = useState(users);
+
+  const handleSearch = (query: string) => {
+    const lowercasedQuery = query.toLowerCase();
+    const filtered = users.filter((user) =>
+      user.title.toLowerCase().includes(lowercasedQuery)
+    );
+    setFilteredUsers(filtered);
+  };
+
   return (
     <DivContainer>
       <Search label="Buscar" onSearch={handleSearch} />
       <div>
-        <H2>usuarios más recientes</H2>
+        <H2>Usuarios más recientes</H2>
         <Carousel />
       </div>
       <div>
-        <H2>todos los usuarios</H2>
-        <AllUsers />
+        <H2>Todos los usuarios</H2>
+        <AllUsers users={filteredUsers} />
       </div>
     </DivContainer>
   );
