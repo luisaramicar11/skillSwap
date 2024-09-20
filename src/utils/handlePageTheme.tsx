20c20eb8cc21e6b54aa83ea4367a07825110069a
David Francisco Blandón Mena <franccoina@gmail.com>
2024-09-20 15:39:44 -0500

    Fixing discover and sidebar functions

--- src/utils/handlePageTheme.tsx
+++ src/utils/handlePageTheme.tsx
@@ -19,3 +19,5 @@ export function handlePageChange(label: string) {
         window.dispatchEvent(new Event('storage'));
     }
 }
+
+
---
+++ src/utils/ourAlertsText.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const OurAlertsText = styled.p`
+  position: fixed;
+  bottom: 74px;
+  padding: 10px;
+  font-size: 14px;
+  border-radius: 10px;
+  font-weight: bold;
+  color: ${({ theme }) => theme.colors.textDark};
+  background-color: ${({ theme }) => theme.colors.textWhite};
+  border: 1px solid ${({ theme }) => theme.colors.textDark};
+`;
