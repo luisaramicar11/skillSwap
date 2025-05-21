ecb699c9adfc630ac1c6bf572c6bb402d03042fb
David Francisco Blandón Mena <franccoina@gmail.com>
2025-05-20 22:29:40 -0500

    Change content from legal sections

--- src/app/legal/page.tsx
+++ src/app/legal/page.tsx
@@ -35,7 +35,7 @@ const Container = styled.div`
 `;
 
 const Title = styled.h1`
-  text-align: start;
+  text-align: center;
   margin: 0;
   background: ${({ theme }) => theme.colors.gradientText};
   font-size: 2.5rem;
@@ -54,10 +54,11 @@ const SectionTitle = styled.h2`
   font-size: 1.5rem;
   color: ${({ theme }) => theme.colors.textSecondary};
   margin-bottom: 10px;
-  font-weight: 600;
+  font-weight: 200;
   display: flex;
   align-items: center;
   gap: 10px;
+  opacity: 0.8;
 `;
 
 const Paragraph = styled.p`
@@ -112,12 +113,12 @@ const Legal = () => {
         <BackLink onClick={() => handlePageChange("INICIO")}>
           <Arrow>&lt;</Arrow> VOLVER A <StyledNavLink href="/" label="INICIO"></StyledNavLink><Arrow>&gt;</Arrow>
         </BackLink>
-        <Title>Información Legal</Title>
+        <Title>Legal</Title>
         <hr />
 
         <Section>
-          <SectionTitle><Image src={skillswap_isotype} alt="skillswap-isotype" width={25} height={25}></Image> Skillswap, Inc</SectionTitle>
-          <Paragraph>Looking for Skill</Paragraph>
+          <SectionTitle><Image src={skillswap_isotype} alt="skillswap-isotype" width={25} height={25}></Image> SkillSwap</SectionTitle>
+          <Paragraph>Looking for skill</Paragraph>
         </Section>
 
         <Section>
@@ -128,14 +129,14 @@ const Legal = () => {
         <Section>
           <SectionTitle>Política de Privacidad</SectionTitle>
           <Paragraph>
-            En Skillswap, nos tomamos muy en serio la protección de tu privacidad. Recolectamos información personal únicamente para ofrecer un mejor servicio y mejorar tu experiencia en nuestra plataforma. Los datos que recopilamos incluyen, pero no se limitan a, nombre, correo electrónico y las habilidades que desees compartir o aprender. Esta información se usa exclusivamente para conectar a los usuarios dentro de la plataforma y no será compartida con terceros sin tu consentimiento.
+            En SkillSwap, nos tomamos muy en serio la protección de tu privacidad. Recolectamos información personal únicamente para ofrecer un mejor servicio y mejorar tu experiencia en nuestra plataforma. Los datos que recopilamos incluyen, pero no se limitan a, nombre, correo electrónico y las habilidades que desees compartir o aprender. Esta información se usa exclusivamente para conectar a los usuarios dentro de la plataforma y no será compartida con terceros sin tu consentimiento.
           </Paragraph>
         </Section>
 
         <Section>
           <SectionTitle>Términos de Uso</SectionTitle>
           <Paragraph>
-            Al utilizar Skillswap, aceptas que la plataforma es un espacio de intercambio de conocimientos, y que el uso indebido, la falta de respeto o cualquier comportamiento perjudicial hacia otros usuarios puede resultar en la cancelación de tu cuenta. Nos reservamos el derecho de modificar estos términos en cualquier momento.
+            Al utilizar SkillSwap, aceptas que la plataforma es un espacio de intercambio de conocimientos, y que el uso indebido, la falta de respeto o cualquier comportamiento perjudicial hacia otros usuarios puede resultar en la cancelación de tu cuenta. Nos reservamos el derecho de modificar estos términos en cualquier momento.
           </Paragraph>
         </Section>
 
@@ -149,35 +150,35 @@ const Legal = () => {
         <Section>
           <SectionTitle>Propiedad Intelectual</SectionTitle>
           <Paragraph>
-            Todos los contenidos publicados en Skillswap, incluidas las imágenes, textos y logotipos, son propiedad de Skillswap o de los respectivos titulares de derechos. Queda prohibido el uso de cualquier material sin el consentimiento explícito del propietario.
+            Todos los contenidos publicados en SkillSwap, incluidas las imágenes, textos y logotipos, son propiedad de SkillSwap o de los respectivos titulares de derechos. Queda prohibido el uso de cualquier material sin el consentimiento explícito del propietario.
           </Paragraph>
         </Section>
 
         <Section>
           <SectionTitle>Limitación de Responsabilidad</SectionTitle>
           <Paragraph>
-            Skillswap es una plataforma que facilita la conexión entre usuarios para el intercambio de conocimientos. No somos responsables de la calidad de la información intercambiada entre los usuarios ni de las interacciones fuera de la plataforma. Skillswap no asume ninguna responsabilidad por daños directos o indirectos derivados del uso del sitio web.
+          SkillSwap es una plataforma que facilita la conexión entre usuarios para el intercambio de conocimientos. No somos responsables de la calidad de la información intercambiada entre los usuarios ni de las interacciones fuera de la plataforma. SkillSwap no asume ninguna responsabilidad por daños directos o indirectos derivados del uso del sitio web.
           </Paragraph>
         </Section>
 
         <Section>
           <SectionTitle>Jurisdicción</SectionTitle>
           <Paragraph>
-            Cualquier disputa que surja en relación con el uso de Skillswap será resuelta conforme a las leyes de la República de Colombia, y cualquier conflicto relacionado con los mismos se someterá a los tribunales competentes del país.
+            Cualquier disputa que surja en relación con el uso de SkillSwap será resuelta conforme a las leyes de la República de Colombia, y cualquier conflicto relacionado con los mismos se someterá a los tribunales competentes del país.
           </Paragraph>
         </Section>
 
         <Section>
-          <SectionTitle>¿Por qué Skillswap?</SectionTitle>
+          <SectionTitle>¿Por qué SkillSwap?</SectionTitle>
           <Paragraph>
-            En el dinámico entorno digital de hoy, la necesidad de actualizar y fortalecer habilidades es crucial para mantener la competitividad. Skillswap ha sido creada para abordar estos desafíos con una solución moderna e innovadora. Nuestra plataforma simplifica el intercambio de conocimientos al conectar a personas que buscan aprender nuevas habilidades con aquellas dispuestas a compartir su experiencia. Creemos firmemente en el poder del aprendizaje colaborativo como motor de crecimiento personal y profesional.
+            En el dinámico entorno digital de hoy, la necesidad de actualizar y fortalecer habilidades es crucial para mantener la competitividad. SkillSwap ha sido creada para abordar estos desafíos con una solución moderna e innovadora. Nuestra plataforma simplifica el intercambio de conocimientos al conectar a personas que buscan aprender nuevas habilidades con aquellas dispuestas a compartir su experiencia. Creemos firmemente en el poder del aprendizaje colaborativo como motor de crecimiento personal y profesional.
           </Paragraph>
         </Section>
 
         <Section>
           <SectionTitle>¿Para Quién?</SectionTitle>
           <Paragraph>
-            Skillswap está diseñada para todos en el entorno digital, desde creativos hasta desarrolladores, estudiantes y profesionales que buscan nuevas competencias. Al unirte a nuestra comunidad, disfrutas de un entorno fresco y estimulante que facilita la enseñanza y el aprendizaje colaborativo de una manera moderna y eficiente.
+          SkillSwap está diseñada para todos en el entorno digital, desde creativos hasta desarrolladores, estudiantes y profesionales que buscan nuevas competencias. Al unirte a nuestra comunidad, disfrutas de un entorno fresco y estimulante que facilita la enseñanza y el aprendizaje colaborativo de una manera moderna y eficiente.
           </Paragraph>
         </Section>
       </Container>
